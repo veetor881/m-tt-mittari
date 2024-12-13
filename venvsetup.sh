ba565738fca50178e960bfd97805f630d0b546ac
veetor881 <veetirivinoja@hotmail.fi>
2024-12-13 14:35:53 +0200

    Update venvsetup.sh

--- venvsetup.sh
+++ venvsetup.sh
@@ -1,10 +1,10 @@
 # Create the virtual environment
 echo "Creating virtual environment..."
-virtualenv ma_env
+virtualenv mättö_env
 
 # Activate the virtual environment
 echo "Activating virtual environment..."
-source ma_env/bin/activate
+source mättö_env/bin/activate
 
 # Install dependencies
 echo "Installing dependencies from requirements.txt..."
